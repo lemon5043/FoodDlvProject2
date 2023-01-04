b23d2c5e16393c83df9aee1518ab0fdea9d4fc79
Bohan Hou <john860703@gmail.com>
2023-01-04 16:18:11 +0800

    測試

---
+++ FoodDlvProject2/Controllers/ButtonController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FoodDlvProject2.Controllers
+{
+    public class ButtonController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
