212bea6b6123cc3374ee82dead11a59bb364ed10
Bohan Hou <john860703@gmail.com>
2023-01-16 16:34:37 +0800

    更新viewmodel

---
+++ FoodDlvProject2/Models/ViewModels/StaffVM.cs
@@ -0,0 +1,17 @@
+﻿namespace FoodDlvProject2.Models.ViewModels
+{
+    public class StaffVM
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Account { get; set; }
+        public string EncryptedPassword { get; set; }
+        public string Title { get; set; }
+        public string Role { get; set; }
+        public DateTime RegistrationTime { get; set; }
+        public byte[] Photo { get; set; }
+        public string Email { get; set; }
+        public DateTime Birthday { get; set; }
+    }
+}
