51326f43369da57b1d7138da19ccf736d17e03a5
Pitzer34 <peter00880@gmail.com>
2023-01-17 02:43:06 +0800

    Order_ver0.7.5

--- FoodDlvProject2/Controllers/OrdersController.cs
+++ FoodDlvProject2/Controllers/OrdersController.cs
@@ -48,68 +48,15 @@ namespace FoodDlvProject2.Controllers
 
 
 		//GET: OrderProducts
-		public IActionResult ProductIndex(long ProductId)
+		public IActionResult ProductDetails(long Id)
 		{
-			var data = orderService.ProductSearch(ProductId)
-				.Select(x => x.ToOrderProductVM());
+			var data = orderService.ProductSearch(Id)
+				.Select(x => x.ToOrderProductVM()).FirstOrDefault();
 
 			return View(data);
-		}
+		}			
 
 		
-		
-
-		//GET(list): Orders/OrderDetails/5
-		//public async Task<IActionResult> Details(long id)
-		//{
-		//    var orderDetail = _context.OrderDetails
-		//        .Include(p => p.Product)
-		//        .Where(x => x.Id == id)
-		//        .Select(x => new OrderDetailVM
-		//        {
-		//            ProductId = x.ProductId,
-		//            ProductName = x.Product.ProductName,
-		//            UnitPrice = x.UnitPrice,
-		//            Count = x.Count,
-		//        });
-
-		//    return View(await orderDetail.ToListAsync());
-		//}
-
-		//GET(detail): Orders/OrderDetails/Products/5
-		//public async Task<IActionResult> ProductsDetail(long id)
-		//{
-		//    var product = await _context.Products
-		//        .Include(s => s.Store)              
-		//        .Select(x => new
-		//        {
-		//            Id = x.Id,
-		//            StoreId = x.StoreId,
-		//            ProductName = x.ProductName,
-		//            Photo = x.Photo,
-		//            ProductContent = x.ProductContent,
-		//        })
-		//        .FirstOrDefaultAsync(x => x.Id == id);
-		//    return View(product);
-		//}
-
-
-		//GET(delete): Orders/Delete/5
-		//public async Task<IActionResult> Delete(long id)
-		//{
-		//    var order = await _context.Orders
-		//        .Include(os => os.OrderSchedules)
-		//        .Include(od => od.OrderDetails)
-		//        .FirstOrDefaultAsync(x => x.Id == id);
-
-		//    return View(order);
-		//}
-
-		////POST(delete): Order/Delete/5
-		//public async Task<IActionResult> DeleteConfirmed(long id)
-		//{
-		//    return View();
-		//}
 
 	}
 }
