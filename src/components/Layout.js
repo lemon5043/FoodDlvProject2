8980c21d9b8f76fe4ef3caa5cfc3e76fa36a7e92
Bohan Hou <john860703@gmail.com>
2023-02-14 20:47:06 +0800

    建立基本頁面

---
+++ src/components/Layout.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Layout = () => {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Layout;
