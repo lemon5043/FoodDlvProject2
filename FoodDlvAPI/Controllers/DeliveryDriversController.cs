5667c09bde98f5426ed963c9f5d8135c09828cc0
Angus <protosscarrier@gamil.com>
2023-02-23 16:34:24 +0800

    測試2
    測試2

--- FoodDlvAPI/Controllers/DeliveryDriversController.cs
+++ FoodDlvAPI/Controllers/DeliveryDriversController.cs
@@ -22,6 +22,8 @@ using System.Security.Claims;
 
 namespace FoodDlvAPI.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class DeliveryDriversController : Controller
     {
         private readonly DeliveryDriverService deliveryDriverService;
@@ -33,79 +35,80 @@ namespace FoodDlvAPI.Controllers
             this.deliveryDriverService = new DeliveryDriverService(repository);
         }
 
-        [HttpPost]
-        public async Task<string> Login(LoginVM model)
-        {
-            LoginResponse response = await deliveryDriverService.Login(model.Account, model.Password);
+        //[HttpPost]
+        //public async Task<string> Login(LoginVM model)
+        //{
+        //    LoginResponse response = await deliveryDriverService.Login(model.Account, model.Password);
 
-            if (response.IsSuccess)
-            {
-                // 記住登入成功的會員，
-                var rememberMe = true;
+        //    if (response.IsSuccess)
+        //    {
+        //        // 記住登入成功的會員，
+        //        var rememberMe = true;
 
-                var member = deliveryDriverService.GetByAccount(model.Account);
-                //string roles = member.Role;
+        //        var member = deliveryDriverService.GetByAccount(model.Account);
+        //        //string roles = member.Role;
 
-                var claims = new List<Claim>
-                {
-                     new Claim(ClaimTypes.Name, member.FirstName),
-                     //new Claim(ClaimTypes.Role, roles),
-                };
+        //        var claims = new List<Claim>
+        //        {
+        //             new Claim(ClaimTypes.Name, member.FirstName),
+        //             //new Claim(ClaimTypes.Role, roles),
+        //        };
 
-                var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+        //        var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
 
-                var authProperties = new AuthenticationProperties
-                {
-                    IsPersistent = rememberMe,
-                    ExpiresUtc = DateTimeOffset.UtcNow.AddDays(3),
-                };
+        //        var authProperties = new AuthenticationProperties
+        //        {
+        //            IsPersistent = rememberMe,
+        //            ExpiresUtc = DateTimeOffset.UtcNow.AddDays(3),
+        //        };
 
-                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
-                new ClaimsPrincipal(claimsIdentity), authProperties);
+        //        await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
+        //        new ClaimsPrincipal(claimsIdentity), authProperties);
 
-                var url = LocalRedirect("/Home/Index");
-                return url.ToString();
-            }
+        //        var url = LocalRedirect("/Home/Index");
+        //        return url.ToString();
+        //    }
 
-            ModelState.AddModelError(string.Empty, response.ErrorMessage);
+        //    ModelState.AddModelError(string.Empty, response.ErrorMessage);
 
-            return response.ErrorMessage;
-        }
+        //    return response.ErrorMessage;
+        //}
 
 
 
-        [AllowAnonymous]
-        public async Task<IActionResult> LogOut()
-        {
+        //[AllowAnonymous]
+        //public async Task<IActionResult> LogOut()
+        //{
 
-            // Clear the existing external cookie
-            await HttpContext.SignOutAsync(
-                CookieAuthenticationDefaults.AuthenticationScheme);
+        //    // Clear the existing external cookie
+        //    await HttpContext.SignOutAsync(
+        //        CookieAuthenticationDefaults.AuthenticationScheme);
 
-            var url = LocalRedirect("/Staffs/Login");
-            return url;
-        }
+        //    var url = LocalRedirect("/Staffs/Login");
+        //    return url;
+        //}
 
-        // GET: DeliveryDrivers/Details/5
-        public async Task<DeliveryDriversDetailsVM> Details(int? id)
+        // GET: api/DeliveryDrivers/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<DeliveryDriversDetailsVM>> Details(int? id)
         {
             var data = await deliveryDriverService.GetOneAsync(id);
             return data.ToDeliveryDriversDetailsVM();
         }
 
         // GET: DeliveryDrivers/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
+        //public IActionResult Create()
+        //{
+        //    return View();
+        //}
 
         //POST: DeliveryDrivers/Create
         //To protect from overposting attacks, enable the specific properties you want to bind to.
         //For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
 
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<string> Create(DeliveryDriverCreateVM deliveryDriver)
+        //[ValidateAntiForgeryToken]
+        public async Task<ActionResult<string>> Create(DeliveryDriverCreateVM deliveryDriver)
         {
             if (ModelState.IsValid)
             {
@@ -121,20 +124,20 @@ namespace FoodDlvAPI.Controllers
             return "輸入資料有誤，請再確認";
         }
 
-        // GET: DeliveryDrivers/Edit/5
-
-        public async Task<DeliveryDriversEditVM> Edit(int? id)
+        // GET: api/DeliveryDrivers/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<DeliveryDriversEditVM>> Edit(int? id)
         {
             var data = await deliveryDriverService.GetEditAsync(id);
             return data.ToDeliveryDriversEditVM();
         }
 
-        // POST: DeliveryDrivers/Edit/5
+        // PUT: api/DeliveryDrivers/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<string> Edit(DeliveryDriversEditVM deliveryDriver)
+        [HttpPut("{id}")]
+        //[ValidateAntiForgeryToken]
+        public async Task<ActionResult<string>> Edit(DeliveryDriversEditVM deliveryDriver)
         {
             if (ModelState.IsValid)
             {
