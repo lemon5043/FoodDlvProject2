be09b5586affc2c9af4dd190ba68812b2d16922a
YLH25 <1402howard@gmail.com>
2023-02-24 16:28:39 +0800

    新增列出所有商店，搜尋功能待修正

--- FoodDlvAPI/Controllers/StoresController.cs
+++ FoodDlvAPI/Controllers/StoresController.cs
@@ -10,17 +10,48 @@ using FoodDlvAPI.DTOs;
 
 namespace FoodDlvAPI.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class StoresController : ControllerBase
-    {
-        private readonly AppDbContext _context;
+	[Route("api/[controller]")]
+	[ApiController]
+	public class StoresController : ControllerBase
+	{
+		private readonly AppDbContext _context;
 
-        public StoresController(AppDbContext context)
-        {
-            _context = context;
+		public StoresController(AppDbContext context)
+		{
+			_context = context;
 		}
 
+		//1列出所有商店
+		// GET: api/Stores
+		[HttpGet]
+		public async Task<ActionResult<IEnumerable<StoreDTO>>> GetStores()
+		{
+			var getAllStores = await _context.Stores.Include(s => s.StoresCategoriesLists).ThenInclude(x => x.Category).Select(x => new StoreDTO
+			{
+				Id = x.Id,
+				StorePrincipalId = x.StorePrincipalId,
+				StoreName = x.StoreName,
+				Address = x.Address,
+				ContactNumber = x.ContactNumber,
+				Photo = x.Photo,
+
+
+				CategoryName = x.StoresCategoriesLists.Select(s => s.Category.CategoryName)
+
+			}).ToListAsync();
+			return getAllStores;
+		}
+
+
+
+
+		//1.1列出所有類別
+		// GET: api/StoreCategories
+		[HttpGet("getStoreCategories")]
+		public async Task<ActionResult<IEnumerable<StoreCategory>>> GetStoreCategories()
+		{
+			return await _context.StoreCategories.ToListAsync();
+		}
 
 
 		//2依類別選出商店，點選類別傳入類別ID顯示商店
@@ -33,10 +64,10 @@ namespace FoodDlvAPI.Controllers
 				var getStores = await _context.Stores.Include(s => s.StoresCategoriesLists).ThenInclude(x => x.Category).Where(x => x.StoresCategoriesLists.Any(scl => scl.CategoryId == CategoryId)).Select(x => new StoreDTO
 				{
 					Id = x.Id,
-					StorePrincipalId=x.StorePrincipalId,
+					StorePrincipalId = x.StorePrincipalId,
 					StoreName = x.StoreName,
-                    Address= x.Address,
-                    ContactNumber= x.ContactNumber,
+					Address = x.Address,
+					ContactNumber = x.ContactNumber,
 					Photo = x.Photo,
 
 					CategoryName = x.StoresCategoriesLists
@@ -73,10 +104,28 @@ namespace FoodDlvAPI.Controllers
 		public async Task<ActionResult<IEnumerable<StoreDTO>>> SearchStore(string? searchString)
 		{
 
+			//var searchStores = await _context.Stores.Include(x => x.StoresCategoriesLists).ThenInclude(x => x.Category).Include(x => x.Products).
+			//			   Where(s => s.StoreName.Contains(searchString)
+			//			   || s.StoresCategoriesLists.Any(scl => scl.Category.CategoryName.Contains(searchString))
+			//			   || s.Products.Any(p => p.ProductName.Contains(searchString))).Select(x => new StoreDTO
+			//			   {
+			//				   Id = x.Id,
+			//				   StorePrincipalId = x.StorePrincipalId,
+			//				   StoreName = x.StoreName,
+			//				   Address = x.Address,
+			//				   ContactNumber = x.ContactNumber,
+			//				   Photo = x.Photo,
+
+			//				   CategoryName = x.StoresCategoriesLists.Select(s => s.Category.CategoryName)
+			//			   }).ToListAsync();
+
+			//return searchStores;
+
+
 			var searchStores = await _context.Stores.Include(x => x.StoresCategoriesLists).ThenInclude(x => x.Category).Include(x => x.Products).
 						   Where(s => s.StoreName.Contains(searchString)
 						   || s.StoresCategoriesLists.Any(scl => scl.Category.CategoryName.Contains(searchString))
-						   || s.Products.Any(p => p.ProductName.Contains(searchString))).Select(x => new StoreDTO
+						   || s.Products.Any(p => p.ProductName.Contains(searchString) && s.Products.Any(p => p.Status == true))).Select(x => new StoreDTO
 						   {
 							   Id = x.Id,
 							   StorePrincipalId = x.StorePrincipalId,
@@ -90,120 +139,53 @@ namespace FoodDlvAPI.Controllers
 
 			return searchStores;
 
+
+
+
+
+
+
+
 		}
 
 
 
+		//3進入商店頁面顯示其資訊包含商品資訊
 
+		[HttpGet("StoreDetail/{StoreId}")]
 
+		public async Task<ActionResult<IEnumerable<StoreDetailDTO>>> GetStoreDetail(int StoreId)
+		{
 
+			var getStores = await _context.Stores.Include(s => s.StoresCategoriesLists).ThenInclude(x => x.Category).Where(x => x.Id == StoreId).Select(x => new StoreDetailDTO
+			{
+				Id = x.Id,
+				StorePrincipalId = x.StorePrincipalId,
+				StoreName = x.StoreName,
+				Address = x.Address,
+				ContactNumber = x.ContactNumber,
+				Photo = x.Photo,
 
+				CategoryName = x.StoresCategoriesLists.Select(s => s.Category.CategoryName)
 
+			   ,
+				ProductId = x.Products.Select(x => x.Id)
+			   ,
+				ProductName = x.Products.Select(x => x.ProductName)
+			   ,
+				ProductPhoto = x.Products.Select(x => x.Photo)
+				,
+				ProductContent = x.Products.Select(x => x.ProductContent)
+				,
+				ProductStatus = x.Products.Select(x => x.Status)
+				,
+				ProductUnitPrice = x.Products.Select(x => x.UnitPrice)
 
+			})
+				  .ToListAsync();
 
+			return getStores;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-		// GET: api/Stores
-		[HttpGet]
-        public async Task<ActionResult<IEnumerable<Store>>> GetStores()
-        {
-            return await _context.Stores.ToListAsync();
-        }
-
-        // GET: api/Stores/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Store>> GetStore(int id)
-        {
-            var store = await _context.Stores.FindAsync(id);
-
-            if (store == null)
-            {
-                return NotFound();
-            }
-
-            return store;
-        }
-
-        // PUT: api/Stores/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutStore(int id, Store store)
-        {
-            if (id != store.Id)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(store).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!StoreExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
-        // POST: api/Stores
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<Store>> PostStore(Store store)
-        {
-            _context.Stores.Add(store);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetStore", new { id = store.Id }, store);
-        }
-
-        // DELETE: api/Stores/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteStore(int id)
-        {
-            var store = await _context.Stores.FindAsync(id);
-            if (store == null)
-            {
-                return NotFound();
-            }
-
-            _context.Stores.Remove(store);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
-        }
-
-        private bool StoreExists(int id)
-        {
-            return _context.Stores.Any(e => e.Id == id);
-        }
-    }
+		}
+	}
 }
