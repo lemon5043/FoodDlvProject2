3d8ef2502bea395217ddee696f1c0e218dd3956a
Bohan Hou <john860703@gmail.com>
2023-03-03 20:50:26 +0800

    add store page(未完成)

---
+++ food-dlv-website/src/components/Store/storeComponent.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+const StoreComponent = ({ data }) => {
+  console.log(data);
+  const image = require(`../../assets/images/public/Stores/${data.photo}`);
+  return (
+    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+      <img src={image} alt="" className="w-full object-cover h-52" />
+      {/* <img class="w-full" src="/img/card-top.jpg" alt="Sunset in the mountains"> */}
+      <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-2">{data.storeName}</div>
+        <p className="text-gray-700 text-base">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
+          quia, nulla! Maiores et perferendis eaque, exercitationem praesentium
+          nihil.
+        </p>
+      </div>
+      <div className="px-6 pt-4 pb-2">
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+          #photography
+        </span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+          #travel
+        </span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+          #winter
+        </span>
+      </div>
+      {/* // <div>
+    //   <div className="item">
+    //     <img */}
+      {/* //       src={image}
+    //       alt=""
+    //       style={{ height: "200px", maxWidth: "300px" }}
+    //     />
+    //   </div> */}
+      {/* // </div> */}
+    </div>
+  );
+};
+
+export default StoreComponent;
--- food-dlv-website/src/pages/Store/Store.js
+++ food-dlv-website/src/pages/Store/Store.js
@@ -1,18 +1,29 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import StoreService from "../../services/store.service";
+import StoreComponent from "../../components/Store/storeComponent";
 
 const Store = () => {
+  let [data, setData] = useState([]);
+  // let [page, setPage] = useState(1);
+  // let [enableLoadMoreData, setEnableLoadMoreData] = useState(false);
+
   const search = async () => {
     let result = await StoreService.getStore();
-    console.log(result.data);
+    setData(result.data);
+    // setEnableLoadMoreData(true);
   };
-  // useEffect(() => {
-  //   search();
-  // }, []);
+  useEffect(() => {
+    search();
+  }, []);
 
   return (
     <div>
       <button onClick={search}>測試</button>
+      <div className="flex flex-wrap">
+        {data.map((d) => {
+          return <StoreComponent data={d} />;
+        })}
+      </div>
     </div>
   );
 };
