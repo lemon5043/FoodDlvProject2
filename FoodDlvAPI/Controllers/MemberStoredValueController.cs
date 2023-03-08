64f59027664d60e2c327ae9b60fec72eebb78a5d
hitto <hitto0316@gmail.com>
2023-03-09 02:27:03 +0800

    金流待今天問老師

---
+++ FoodDlvAPI/Controllers/MemberStoredValueController.cs
@@ -0,0 +1,101 @@
+﻿using FoodDlvAPI.Interfaces;
+using FoodDlvAPI.Models.EcPayModel;
+using FoodDlvAPI.Models.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+namespace FoodDlvAPI.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class MemberStoredValueController : Controller
+	{
+		private readonly ILogger<MemberStoredValueController> _logger;
+		private readonly IConfiguration Config;
+
+		public MemberStoredValueController(ILogger<MemberStoredValueController> logger)
+		{
+			_logger = logger;
+			Config = new ConfigurationBuilder().AddJsonFile("appSettings.json").Build();
+		}
+
+		private ICommerce GetPayType(string option)
+		{
+			switch (option)
+			{
+				
+				case "ECPay":
+					return new ECPayService();
+
+				default: throw new ArgumentException("No Such option");
+			}
+		}
+
+		private string GetReturnValue(ICommerce service, SendToNewebPayIn inModel)
+		{
+			switch (inModel.PayOption)
+			{
+				
+				case "ECPay":
+					return service.GetCallBack(inModel);
+
+				default: throw new ArgumentException("No Such option");
+			}
+		}
+
+		public IActionResult Index()
+		{
+			ViewData["MerchantOrderNo"] = DateTime.Now.ToString("yyyyMMddHHmmss");  //訂單編號
+			ViewData["ExpireDate"] = DateTime.Now.AddDays(3).ToString("yyyyMMdd"); //繳費有效期限       
+			return View();
+		}
+
+		public IActionResult SendToNewebPay(SendToNewebPayIn inModel)
+		{
+			var service = GetPayType(inModel.PayOption);
+
+			return Json(GetReturnValue(service, inModel));
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> GetReturn(SendToNewebPayIn inModel)
+		{
+			var obj = await new ECPayService().GetQueryCallBack(inModel.MerchantOrderNo, inModel.Amt);
+			return Json(obj);
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <returns></returns>
+
+		/// <summary>
+		/// 支付完成返回網址
+		/// </summary>
+		/// <returns></returns>
+		public IActionResult CallbackReturn(string option)
+		{
+			var service = GetPayType(option);
+			var result = service.GetCallbackResult(Request.Form);
+			ViewData["ReceiveObj"] = result.ReceiveObj;
+			ViewData["TradeInfo"] = result.TradeInfo;
+
+			return View();
+		}
+
+
+		/// <summary>
+		/// 商店取號網址
+		/// </summary>
+		/// <returns></returns>
+		public IActionResult CallbackCustomer(string option)
+		{
+			var service = GetPayType(option);
+			var result = service.GetCallbackResult(Request.Form);
+			ViewData["ReceiveObj"] = result.ReceiveObj;
+			ViewData["TradeInfo"] = result.TradeInfo;
+			return View();
+		}
+
+	}
+}
---
+++ FoodDlvAPI/Controllers/NotifyController.cs
@@ -0,0 +1,75 @@
+﻿using FoodDlvAPI.Interfaces;
+using FoodDlvAPI.Models.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+
+namespace FoodDlvAPI.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class NotifyController : ControllerBase
+	{//選擇綠界系統
+		private ICommerce GetPayType(string option)
+		{
+			switch (option)
+			{
+				case "ECPay":
+					return new ECPayService();
+
+				default: throw new ArgumentException("No Such option");
+			}
+		}
+
+		[HttpGet]
+		public bool GetType()
+		{
+			return true;
+		}
+
+		/// <summary>
+		/// 支付通知網址
+		/// </summary>
+		/// <returns></returns>
+		public HttpResponseMessage CallbackNotify(string bank)
+		{
+			var service = GetPayType(bank);
+			var result = service.GetCallbackResult(Request.Form);
+
+			//TODO 支付成功後 可做後續訂單處理
+
+			return ResponseOK();
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <returns></returns>
+		[HttpPost]
+
+		/// <summary>
+		/// 回傳給 綠界 失敗
+		/// </summary>
+		/// <returns></returns>
+		private HttpResponseMessage ResponseError()
+		{
+			var response = new HttpResponseMessage();
+			response.Content = new StringContent("0|Error");
+			response.Content.Headers.ContentType = new MediaTypeHeaderValue("text/html");
+			return response;
+		}
+
+		/// <summary>
+		/// 回傳給 綠界 成功
+		/// </summary>
+		/// <returns></returns>
+		private HttpResponseMessage ResponseOK()
+		{
+			var response = new HttpResponseMessage();
+			response.Content = new StringContent("1|OK");
+			response.Content.Headers.ContentType = new MediaTypeHeaderValue("text/html");
+			return response;
+		}
+	}
+}
+
