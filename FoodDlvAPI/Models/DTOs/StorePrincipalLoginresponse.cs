ff69ea591a74ef5fc1b0ba20190e4f2dab325ae3
YLH25 <1402howard@gmail.com>
2023-03-09 11:03:00 +0800

    遷出分支後微調整

---
+++ FoodDlvAPI/Models/DTOs/StorePrincipalLoginresponse.cs
@@ -0,0 +1,16 @@
+﻿namespace FoodDlvAPI.Models.DTOs
+{
+	public class StorePrincipalLoginresponse
+	{
+		public bool IsSuccess { get; set; }
+		public string ErrorMessage { get; set; }
+		public int Id { get; set; }
+		public string Username { get; set; }
+
+		public static StorePrincipalLoginresponse Success(int id, string userName)
+			=> new StorePrincipalLoginresponse { IsSuccess = true, Id = id, Username = userName };
+
+		public static StorePrincipalLoginresponse Fail(string errorMessage)
+			=> new StorePrincipalLoginresponse { IsSuccess = false, ErrorMessage = errorMessage };
+	}
+}
---
+++ FoodDlvAPI/Models/ViewModels/StorePrincipalLoginVM.cs
@@ -0,0 +1,8 @@
+﻿namespace FoodDlvAPI.Models.ViewModels
+{
+	public class StorePrincipalLoginVM
+	{
+		public string Account { get; set; }
+		public string Password { get; set; }
+	}
+}
